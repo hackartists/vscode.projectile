d3e25e48e3c1fa7891ee6fc1102a32be8f61d286
hackartist <js.choi.85@gmail.com>
2021-01-20 22:37:39 +0900

    init projectile plugin * List projects * List files in project

---
+++ src/extension.ts
@@ -0,0 +1,68 @@
+
+'use strict';
+
+import * as vscode from 'vscode';
+import { workspace } from 'vscode';
+const findGitRoot = require('find-git-root');
+import * as cp from 'child_process';
+import { getVSCodeDownloadUrl } from 'vscode-test/out/util';
+const gitLsFiles = require('git-ls-files')
+
+let projects: any = {};
+
+export function activate(context: vscode.ExtensionContext) {
+    const stateKey = "projectile"
+    const stateKeyCurrentProject = "current-project"
+
+    projects = context.globalState.get(stateKey) ? context.globalState.get(stateKey) : {};
+    context.globalState.setKeysForSync([stateKey])
+
+    const openProjectFiles = (cwd: any) => {
+        const files = gitLsFiles({ cwd })
+        const projectFiles = vscode.window.createQuickPick()
+        projectFiles.items = files.all.map((label: any) => ({ label }))
+        projectFiles.canSelectMany = false
+        projectFiles.onDidChangeSelection(async (fileItem) => {
+            const file = fileItem[0].label
+            vscode.window.showInformationMessage(`${file} was selected`)
+            projectFiles.hide()
+            const doc = await vscode.workspace.openTextDocument(`${cwd}/${file}`)
+            vscode.window.showTextDocument(doc)
+        })
+        projectFiles.show()
+
+    }
+    vscode.commands.registerCommand('projectile.projectFiles', async () => {
+        const root = vscode.window.activeTextEditor ? findGitRoot(vscode.window.activeTextEditor.document.fileName).replace(/\/\.git$/, "") : undefined
+
+        if (root) {
+            openProjectFiles(root)
+        }
+    })
+
+    vscode.commands.registerCommand('projectile.projects', async () => {
+        const quickPick = vscode.window.createQuickPick()
+        quickPick.items = Object.keys(projects).map(label => ({ label }))
+        quickPick.canSelectMany = false
+        quickPick.onDidChangeSelection((e) => {
+            openProjectFiles(e[0].label)
+            quickPick.hide()
+        })
+
+        quickPick.show()
+    })
+
+    vscode.workspace.onDidOpenTextDocument(async (doc: vscode.TextDocument) => {
+        if (doc.fileName) {
+            try {
+                const root = findGitRoot(doc.fileName).replace(/\/\.git$/, "")
+                projects[root] = true
+                context.globalState.update(stateKey, projects)
+                context.workspaceState.update(stateKeyCurrentProject, root)
+                vscode.window.showInformationMessage(root)
+                workspace.updateWorkspaceFolders(workspace.workspaceFolders ? workspace.workspaceFolders.length : 0, null, { uri: vscode.Uri.file(root) });
+            } catch (e) {
+            }
+        }
+    })
+}
